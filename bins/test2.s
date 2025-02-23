7d7a8d65cc3184a36b1a9159602922614e9ae311
Your Name <cbakewell1@gmail.com>
2025-02-23 03:13:11 -0500

    finally fixed gitignore?

--- bins/test2.s
+++ bins/test2.s
@@ -5,13 +5,16 @@
     .ent    __start
     .type   __start, @function
 __start:
-   addi $0, $2, 9
-   addi $0, $3, 5
-   addi $0, $4, 9
-   addi $0, $5, 5
-   addi $0, $6, 9
+   addi $4, $0, 7
+   add $5, $4
+   add $6, $4
+   addi $0, $0, 0
+   addi $0, $0, 0
+   addi $0, $0, 0
+   addi $0, $0, 0
+   addi $0, $0, 0
 
-label:
-   or $4, $5, $6
+#label:
+#   or $4, $5, $6
     .end    __start
     .size   __start, .-__start
